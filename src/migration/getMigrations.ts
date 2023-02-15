c9204a91becc1af52b214689902b8abb93659244
bhoudu <benjamin.houdu@eulerhermes.com>
2023-02-15 17:37:35 +0100

    :zap: add getMigrations

---
+++ src/migration/getMigrations.ts
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import { Client } from 'cassandra-driver';
+
+export async function getMigrations(
+  client: Client,
+): Promise<any> {
+  const query = fs.readFileSync('resources/cql/getMigration.cql', 'utf8');
+  const result = await client.execute(query, null, { prepare: true });
+  const filesRan = {};
+  for (let i = 0; i < result.rows.length; i++) {
+    filesRan[result.rows[i].migration_number] = (result.rows[i].file_name);
+  }
+  return filesRan;
+}
