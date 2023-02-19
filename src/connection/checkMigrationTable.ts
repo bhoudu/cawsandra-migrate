2153c41bc12645724175db5e4318e6243b445269
Benjamin Houdu <benjamin.houdu@gmail.com>
2023-02-19 23:49:01 +0100

    :zap: fix check migration table

--- src/connection/checkMigrationTable.ts
+++ src/connection/checkMigrationTable.ts
@@ -1,29 +1,19 @@
-import fs from 'fs';
 import { Client } from "cassandra-driver";
-
-const createMigrationTableCheckQuery = fs.readFileSync("resources/cql/createMigrationTableCheck.cql", "utf8");
 
 export async function checkMigrationTable(
   client: Client,
   keyspace: string,
 ): Promise<boolean> {
   try {
-    console.log(`Checking migration table status for keyspace ${keyspace}`)
-    const response = await client.execute(
-      createMigrationTableCheckQuery,
-      {
-        'keyspace_name': keyspace
-      },
-      {
-        prepare: true
-      }
-    );
-    console.log(
-      `Migration table status is: ${response.rows[0]?.status}`
-    );
+    const query = `SELECT keyspace_name, table_name, status
+                   FROM system_schema_mcs.tables
+                   WHERE keyspace_name = ${keyspace}
+                     AND table_name = 'sys_cassandra_migrations'`;
+    console.log(`Checking migration table status for keyspace ${keyspace}`);
+    const response = await client.execute(query);
+    console.log(`Migration table status is: ${response.rows[0]?.status}`);
     return response.rows[0]?.status === "ACTIVE";
   } catch (e) {
-    console.error('Error while checking migration table status');
     console.error(e);
     return false;
   }
